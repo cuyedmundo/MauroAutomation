5b15fc4ed209094a2b24bbe8958553b14d099e71
Mauro <edmundo.vmv@gmail.com>
2025-04-30 18:54:12 -0500

    clean dummy file

--- leraning-notes/dummy.ts
+++ leraning-notes/dummy.ts
@@ -1,9 +1,1 @@
-// Tuples
-let person: [string, number];
-person = ["Mauro", 34];
-person.push("comething");
-
-
-console.log (person);
-console.log (person[0]);
-console.log (person[1]);
+// Dummy file just to run some test
